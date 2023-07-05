515de7a186a9e0a01afc2709afa605608ea6e3be
josh renton <jrenton03@gmail.com>
2023-07-05 23:39:34 +0100

    adds Pawn class

--- src/main/java/pieces/Bishop.java
+++ src/main/java/pieces/Bishop.java
@@ -7,6 +7,11 @@ public class Bishop extends Piece {
         super(isWhite);
     }
 
+    /**
+     * @param startSquare the square the bishop is currently occupying
+     * @param endSquare the square that the bishop is trying to move to
+     * @return true if the bishop can move to that square, ignoring other pieces
+     */
     public boolean canMove(Square startSquare, Square endSquare) {
         int rowDiff = startSquare.getRow() - endSquare.getRow();
         int columnDiff = startSquare.getColumn() - endSquare.getColumn();
---
+++ src/main/java/pieces/Pawn.java
@@ -0,0 +1,33 @@
+package pieces;
+
+import board.Square;
+
+public class Pawn extends Piece{
+    private boolean hasMoved;
+
+    public Pawn(boolean isWhite) {
+        super(isWhite);
+        this.hasMoved = false;
+    }
+
+    public boolean canMove(Square startSquare, Square endSquare) {
+        int rowDiff = endSquare.getRow() - startSquare.getRow();
+        int columnDiff = endSquare.getColumn() - startSquare.getColumn();
+
+        if (columnDiff == 0 && rowDiff == 1) { // Pawn moving forward one square.
+            return true;
+        } else if (columnDiff == 0 && rowDiff == 2 && !hasMoved) { // First pawn move can be two squares.
+            return true;
+        } else if (!endSquare.isOccupied()) {
+            return false;
+        } else if (Math.abs(columnDiff) == 1 && rowDiff == 1 && endSquare.getPiece().isWhite() != this.isWhite()) { // Pawn can only move diagonally if capturing.
+            return true;
+        }
+
+        return false;
+    }
+
+    public void setMoved() {
+        hasMoved = true;
+    }
+}
