64eaa9b28c43d7686a165e7003dd263837c70397
josh renton <jrenton03@gmail.com>
2023-07-23 10:47:37 +0100

    prints row and column markers

--- src/main/java/engine/GameEngine.java
+++ src/main/java/engine/GameEngine.java
@@ -1,8 +1,7 @@
-import board.Board;
-import pieces.Pawn;
-import pieces.Piece;
+package engine;
 
-import java.util.Optional;
+import board.Board;
+import pieces.Piece;
 
 public class GameEngine {
     private static Board board;
@@ -19,6 +18,8 @@ public class GameEngine {
 
     private static void printBoard() {
         for (int row = BOARD_SIZE - 1; row >= 0; row--) {
+            System.out.print(row + 1);
+            System.out.print(" | ");
             for (int column = 0; column < BOARD_SIZE; column++) {
                 Piece piece = board.getPiece(row, column);
 
@@ -33,6 +34,20 @@ public class GameEngine {
                 }
             }
             System.out.println();
+        }
+
+        System.out.print("    ");
+        for (int column = 0; column < BOARD_SIZE; column++) {
+            System.out.print('_');
+            System.out.print(' ');
+        }
+        System.out.println();
+
+        // Prints column letters along the bottom of the board.
+        System.out.print("    ");
+        for (char column = 'a'; column <= 'h'; column++) {
+            System.out.print(column);
+            System.out.print(' ');
         }
     }
 }
